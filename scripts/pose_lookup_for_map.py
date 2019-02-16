f2825bb9da1f987bf19b96bbc6d48c38f58093c4
Benjamin Perseghetti <perseghetti@gmail.com>
2019-02-16 14:41:29 -0500

    new config

--- scripts/pose_lookup_for_map.py
+++ scripts/pose_lookup_for_map.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-
 import os
 import sys
 import copy
@@ -67,13 +66,13 @@ def toEA_RPY(qt):
     return([R, P, Y])
 
 
-def easyDEG_RPY(RPY):
+def easyDEG_RPY(RPY, fix_roll):
     if len(RPY) == 6:
-        Rdeg, Pdeg, Ydeg = [(180/m.pi)*RPY[3],(180/m.pi)*RPY[4],(180/m.pi)*RPY[5]]
+        Rdeg, Pdeg, Ydeg = [(180.0/m.pi)*RPY[3],(180.0/m.pi)*RPY[4],(180.0/m.pi)*RPY[5]]
     elif len(RPY) == 3:
-        Rdeg, Pdeg, Ydeg = [(180/m.pi)*RPY[0],(180/m.pi)*RPY[1],(180/m.pi)*RPY[2]]
+        Rdeg, Pdeg, Ydeg = [(180.0/m.pi)*RPY[0],(180.0/m.pi)*RPY[1],(180.0/m.pi)*RPY[2]]
     Rdeg_mod = 0.0
-    if True:
+    if fix_roll:
         if Rdeg > 0:
             Rdeg_mod = Rdeg-180
         elif Rdeg < 0:
@@ -97,13 +96,17 @@ class Map_Locate(object):
         #JSON providing for dictionary lookup of Apriltag IDs to pose in map, assumption 
         #is standard coordinates for map points but any can be used with map_coord_system param
         apriltag_map_file = rospy.get_param("~apriltag_map_json", 'testaprilpose.json')
-        #camera is in FRD from apriltag3_ros, where Z is forward, X is right, Y is down
-        #below rotations and offsets can be performed in quaternion pose to locate camera from device
-        #self.cam_to_dev = np.fromstring(rospy.get_param("~camera_to_device_pose",'[0.0,0.0,0.0,0.0,0.0,0.0]'))
+        #Camera is in FRD from apriltag3_ros, where Z is forward, X is right, Y is down
+        #But we pull in device pose update in map pose where Y is forward, X is right, Z is up (standard coordinates)
+        self.cam_to_dev = np.fromstring(rospy.get_param("~camera_to_device_poser","0.0,0.0,0.0"), sep=',').astype(float)
         #map is assumed to be in standard coord, but using this any coord system can be implemented **TODO**
         self.map_coord = rospy.get_param("~map_coord_system",'standard')
+        
+        self.debug_PoseDeg = bool(rospy.get_param("~debug_ApriltagMapPoseDeg",'false'))
+
         rospack = rospkg.RosPack()
         self.pkg_path = rospack.get_path('apriltag_map_service')
+
         apriltag_map_json_path = os.path.join(self.pkg_path, 'ipython', apriltag_map_file)
         with open(apriltag_map_json_path, 'r') as atmpf:
             self.AprilMapPoseDict = json.load(atmpf)
@@ -131,28 +134,34 @@ class Map_Locate(object):
     def apriltag_ID_to_map(self, atrd, apriltag_id):
         apriltag_key = "TAG_{}".format(apriltag_id)
         aptg_rd_pos = [atrd.position.x, atrd.position.y, atrd.position.z, atrd.orientation.x ,atrd.orientation.y, atrd.orientation.z, atrd.orientation.w]
-        #print("Found Apriltag Label Map Location: {}".format(self.AprilMapPoseDict[apriltag_key]))
+        AprilMapLabel = self.AprilMapPoseDict[apriltag_key]
         if self.map_coord == 'standard':
-            ApriltagReadMapTransM = [-aptg_rd_pos[0],-aptg_rd_pos[2],aptg_rd_pos[1]]
+            ApriltagReadMapTransM = [-aptg_rd_pos[0]+self.cam_to_dev[0],-aptg_rd_pos[2]+self.cam_to_dev[1],aptg_rd_pos[1]+self.cam_to_dev[2]]
             ApriltagReadRotRad = toEA_RPY(aptg_rd_pos[3:])
-            ApriltagReadRotDeg = easyDEG_RPY(ApriltagReadRotRad)
+            ApriltagReadRotDeg = easyDEG_RPY(ApriltagReadRotRad, True)
             ApriltagReadMapRotRad = [-ApriltagReadRotRad[0], -ApriltagReadRotRad[2], ApriltagReadRotRad[1]]
-            ApriltagReadMapRotDeg = easyDEG_RPY(ApriltagReadMapRotRad)
+            ApriltagReadMapRotDeg = easyDEG_RPY(ApriltagReadMapRotRad, False)
             ApriltagReadMapRotQt = toQt_xyzw(ApriltagReadMapRotRad)
-
-        #print("Apriltag Read Pose no Map Coord: {},{}".format(aptg_rd_pos[0:3], ApriltagReadRotDeg))
-        print("Apriltag Read Pose Map Coord: [{:.3f}, {:.3f}, {:.3f}, {:.3f}, {:.3f}, {:.3f}]".format(ApriltagReadMapTransM[0],ApriltagReadMapTransM[1],ApriltagReadMapTransM[2],ApriltagReadMapRotDeg[0],ApriltagReadMapRotDeg[1],ApriltagReadMapRotDeg[2]))
+            ApriltagMapPoseRad = [AprilMapLabel[0]-(m.sin(AprilMapLabel[5])*ApriltagReadMapTransM[0]), 
+                                AprilMapLabel[1]-(m.sin(AprilMapLabel[5])*ApriltagReadMapTransM[1]), 
+                                AprilMapLabel[2]+ApriltagReadMapTransM[2], 
+                                ApriltagReadMapRotRad[0], ApriltagReadMapRotRad[1], ApriltagReadMapRotRad[2]+(m.pi/2.0+AprilMapLabel[5])]
+            ApriltagMapRotDeg = easyDEG_RPY(ApriltagMapPoseRad, False)
+            ApriltagMapRotQt = toQt_xyzw(ApriltagMapPoseRad)
+            ApriltagMapPoseQt = [ApriltagMapPoseRad[0],ApriltagMapPoseRad[1],ApriltagMapPoseRad[2],ApriltagMapRotQt[0],ApriltagMapRotQt[1],ApriltagMapRotQt[2],ApriltagMapRotQt[3]]
+        
+        if self.debug_PoseDeg:
+        	print("Apriltag Map Pose Deg: [X:{:.2f} m, Y:{:.2f} m, Z:{:.2f} m, R:{:.2f} Deg, P:{:.3f} Deg, Y:{:.3f} Deg]".format(ApriltagMapPoseRad[0],ApriltagMapPoseRad[1],ApriltagMapPoseRad[2],ApriltagMapRotDeg[0],ApriltagMapRotDeg[1],ApriltagMapRotDeg[2]))
+        
         aptg_map_msg = geometry_msgs.msg.Pose()
-        aptg_map_msg.position.x = ApriltagReadMapTransM[0]
-        aptg_map_msg.position.y = ApriltagReadMapTransM[1]
-        aptg_map_msg.position.z = ApriltagReadMapTransM[2]
-        aptg_map_msg.orientation.x = ApriltagReadMapRotQt[0]
-        aptg_map_msg.orientation.y = ApriltagReadMapRotQt[1]
-        aptg_map_msg.orientation.z = ApriltagReadMapRotQt[2]
-        aptg_map_msg.orientation.w = ApriltagReadMapRotQt[3]
+        aptg_map_msg.position.x = ApriltagMapPoseQt[0]
+        aptg_map_msg.position.y = ApriltagMapPoseQt[1]
+        aptg_map_msg.position.z = ApriltagMapPoseQt[2]
+        aptg_map_msg.orientation.x = ApriltagMapPoseQt[3]
+        aptg_map_msg.orientation.y = ApriltagMapPoseQt[4]
+        aptg_map_msg.orientation.z = ApriltagMapPoseQt[5]
+        aptg_map_msg.orientation.w = ApriltagMapPoseQt[6]
         return aptg_map_msg
-
-
 
 def main(args):
     "main function"
